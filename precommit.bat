e85adc546875fd08cfe44a5c608871dc18d2e066
NmdSnprEnigma <NmdSnprEnigma@gmail.com>
2012-09-14 22:08:00 -0400

    Adding a precommit.bat for scrubbing non-xml changes.

---
+++ precommit.bat
@@ -0,0 +1,21 @@
+echo off
+setlocal ENABLEDELAYEDEXPANSION
+::for /f %%f in ('dir /s /b .') do call git update-index -q --no-assume-unchanged %%f
+echo %cd%
+set pwd=%cd%
+cd ComponentList.SC2Map
+echo %cd%
+for /R %%f in (*) do (
+  SET _ext=%%f
+  SET _result=!_ext:~-4!
+  if NOT x!_result! == x.xml ( 
+    if NOT x!_result! == xgers (
+      ECHO Resetting %%f...
+      call git reset HEAD %%f
+      call git checkout -- %%f 
+    )
+  )  
+)
+call git status
+cd %pwd%
+echo Done!
